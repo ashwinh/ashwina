bcec190559d065d1269d52fbeb900aa05bfb0803
ashwinh <35991970+ashwinh@users.noreply.github.com>
2018-02-02 16:18:32 +0530

    a5.c
    largest number among three numbers entered by user

---
+++ a5.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<conio.h>
+void main()
+{
+int a,b,c;
+printf("enter the number a\n");
+scanf("%d",&a);
+printf("enter the number b\n");
+scanf("%d",&b);
+printf("enter the number c\n");
+scanf("%d",&c);
+if(a>b&&a>c)
+{
+printf("enter the number a is greater%d\n");
+}
+if else(b<c)
+{
+printf("enter the number b is greater%d\n");
+}
+else
+{
+printf("enter the number c is greater%d\n");
+}
+getch();
+}
+
+
