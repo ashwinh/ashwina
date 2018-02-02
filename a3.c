bf2d91761b516dc817bf23182d0c1aab7db4830d
ashwinh <35991970+ashwinh@users.noreply.github.com>
2018-02-02 15:48:02 +0530

    a3.c
    vowels or constant

---
+++ a3.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+#include<conio.h>
+int main()
+{
+char vowel;
+printf("enter the letters");
+scanf("%c",&vowel);
+if(vowel=='a'!!vowel=='e'!!vowel=='i'!!vowel=='o'!!vowel=='u')
+printf("vowels");
+else
+printf("constant");
+gerch();
+}
